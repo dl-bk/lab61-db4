c857aef6aa028c5c50587cd22b612aa4743f2fc0
dl-bk <BahtX_hn46@student.itstep.org>
2024-01-17 20:24:12 +0100

    Wards In Department Intensive Treatment

--- query1.sql
+++ query1.sql
@@ -1,4 +1,4 @@
-SELECT Surname AS DoctorSurname, Salary AS TotalSalary
-FROM Doctors
-WHERE NOT EXISTS(SELECT * FROM Vacations WHERE Vacations.doctor_id = Doctors.id AND 
-				CURRENT_DATE BETWEEN start_date AND end_date)
+SELECT W.name AS WardName
+From Wards W
+JOIN Departments D ON W.Building = D.Building
+WHERE D.Name = 'Oncology'
