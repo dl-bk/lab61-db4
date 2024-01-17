8f8129cad67f59e8996779bd9d3df01d34560b96
dl-bk <BahtX_hn46@student.itstep.org>
2024-01-17 20:11:51 +0100

    doctors full names and specializations

--- query1.sql
+++ query1.sql
@@ -1,9 +1,5 @@
--- Add a relationship between wards and departments
-
-ALTER TABLE Wards
-ADD COLUMN Department_Id INT REFERENCES Departments(Id);
-
--- Add bonus to doctors
-
-ALTER TABLE Doctors
-ADD COLUMN Bonus DECIMAL(10, 2) NOT NULL DEFAULT 0 CHECK (Bonus >= 0);
+SELECT Doctors.name || ' ' || Doctors.surname AS DoctorName, string_agg(Specializations.name, ', ') AS Specialization
+FROM Doctors
+JOIN doctorsspecializations ON Doctors.id = doctorsspecializations.doctor_id
+JOIN Specializations ON doctorsspecializations.specialization_id = Specializations.id
+GROUP BY Doctors.Id
